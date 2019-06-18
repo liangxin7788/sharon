de80e380a04686ce7dff7b9025c1828a9b60ab02
liangxin7788 <lx_4509@163.com>
2019-06-18 11:43:40 +0800

    Excel操作

---
+++ src/main/java/com/fun/business/sharon/utils/ExcelPoiUtil.java
@@ -0,0 +1,135 @@
+package com.fun.business.sharon.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.util.ResourceUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.poi.ss.usermodel.CellType.NUMERIC;
+import static org.apache.poi.ss.usermodel.CellType.STRING;
+
+/**
+ * @Package: com.fun.business.sharon.utils
+ * @ClassName: ExcelPoiUtil
+ * @Description: Excel导入导出工具类，将文件/模板放到resources目录下使用
+ * @Author: liangxin
+ * @CreateDate: 2019/6/14 11:07
+ * @UpdateDate: 2019/6/14 11:07
+ */
+@Slf4j
+public class ExcelPoiUtil {
+
+    public static List<String[]> readExcel(String filename) throws IOException {
+        //检查文件
+//        checkFile(file);
+        //获得Workbook工作薄对象
+        Workbook workbook = getWorkBook(filename);
+        //创建返回对象，把每行中的值作为一个数组，所有行作为一个集合返回
+        List<String[]> list = new ArrayList<String[]>();
+        if (workbook != null) {
+            // 若是读取第二个sheet，则此处的sheetNum=1开始
+            for (int sheetNum = 0; sheetNum < workbook.getNumberOfSheets(); sheetNum++) {
+                //获得当前sheet工作表
+                Sheet sheet = workbook.getSheetAt(sheetNum);
+                if (sheet == null) {
+                    continue;
+                }
+                //获得当前sheet的开始行
+                int firstRowNum = sheet.getFirstRowNum();
+                //获得当前sheet的结束行
+                int lastRowNum = sheet.getLastRowNum();
+                //循环除了第一行的所有行
+                for (int rowNum = firstRowNum + 1; rowNum <= lastRowNum; rowNum++) {
+                    //获得当前行
+                    Row row = sheet.getRow(rowNum);
+                    if (row == null) {
+                        continue;
+                    }
+                    //获得当前行的开始列
+                    int firstCellNum = row.getFirstCellNum();
+                    //获得当前行的列数
+//                    int lastCellNum = row.getPhysicalNumberOfCells();
+                    int lastCellNum = sheet.getRow(firstRowNum).getPhysicalNumberOfCells();
+                    String[] cells = new String[lastCellNum];
+                    //循环当前行
+                    for (int cellNum = firstCellNum; cellNum < lastCellNum; cellNum++) {
+                        Cell cell = row.getCell(cellNum);
+                        cells[cellNum] = getCellValue(cell);
+                    }
+                    list.add(cells);
+                }
+            }
+            workbook.close();
+        }
+        return list;
+    }
+
+    public static Workbook getWorkBook(String fileName) {
+        //获得文件名
+//        String fileName = file.getOriginalFilename();
+        //创建Workbook工作薄对象，表示整个excel
+        Workbook workbook = null;
+        try {
+            //获取excel文件的io流
+            InputStream is = new FileInputStream(ResourceUtils.getFile("classpath:"+fileName));
+            //根据文件后缀名不同(xls和xlsx)获得不同的Workbook实现类对象
+            if (fileName.endsWith("xls")) {
+                //2003
+                workbook = new HSSFWorkbook(is);
+            } else if (fileName.endsWith("xlsx")) {
+                //2007
+                workbook = new XSSFWorkbook(is);
+            }
+        } catch (IOException e) {
+            log.info(e.getMessage());
+        }
+        return workbook;
+    }
+
+    public static String getCellValue(Cell cell) {
+        String cellValue = "";
+        if (cell == null) {
+            return cellValue;
+        }
+        //把数字当成String来读，避免出现1读成1.0的情况
+        if (cell.getCellTypeEnum() == NUMERIC) {
+            cell.setCellType(STRING);
+        }
+        //判断数据的类型
+        switch (cell.getCellTypeEnum()) {
+            case NUMERIC: //数字
+                cellValue = String.valueOf(cell.getNumericCellValue());
+                break;
+            case STRING: //字符串
+                cellValue = String.valueOf(cell.getStringCellValue());
+                break;
+            case BOOLEAN: //Boolean
+                cellValue = String.valueOf(cell.getBooleanCellValue());
+                break;
+            case FORMULA: //公式
+                cellValue = String.valueOf(cell.getCellFormula());
+                break;
+            case BLANK: //空值
+                cellValue = "";
+                break;
+            case ERROR: //故障
+                cellValue = "非法字符";
+                break;
+            default:
+                cellValue = "未知类型";
+                break;
+        }
+        return cellValue;
+    }
+
+}
---
+++ src/test/java/com/fun/business/sharon/InterfaceTest/TestController.java
@@ -0,0 +1,42 @@
+package com.fun.business.sharon.InterfaceTest;
+
+import com.fun.business.sharon.SharonApplication;
+import com.fun.business.sharon.common.GlobalResult;
+import com.fun.business.sharon.utils.ExcelPoiUtil;
+import com.fun.business.sharon.utils.ObjectUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @Package: com.fun.business.sharon.InterfaceTest
+ * @ClassName: TestController
+ * @Description: 接口测试专用
+ * @Author: liangxin
+ * @CreateDate: 2019/6/14 14:08
+ * @UpdateDate: 2019/6/14 14:08
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SharonApplication.class)
+public class TestController {
+
+    @Test
+    public void testExcel() throws IOException {
+        List<String[]> excel = ExcelPoiUtil.readExcel("ceshi");
+        if (ObjectUtil.isNotEmpty(excel)) {
+            for (String[] obj : excel) {
+                String mainSku = obj[0];
+                String title = obj[1];
+                String description = obj[2];
+                System.out.println("mainSku:" + mainSku);
+                System.out.println("title:" + title);
+                System.out.println("description:" + description);
+            }
+        }
+    }
+
+}
