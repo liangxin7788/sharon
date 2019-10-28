1c31b14b59f19956413892baa1e63a2b250da476
liangxin7788 <lx_4509@163.com>
2019-10-28 10:56:11 +0800

    暂时屏蔽MQ

--- src/main/java/com/fun/business/sharon/biz/business/controller/UserController.java
+++ src/main/java/com/fun/business/sharon/biz/business/controller/UserController.java
@@ -1,26 +1,11 @@
 package com.fun.business.sharon.biz.business.controller;
 
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.fun.business.sharon.biz.business.bean.User;
 import com.fun.business.sharon.biz.business.service.UserService;
-import com.fun.business.sharon.biz.mq.Queues;
-import com.fun.business.sharon.biz.mq.RabbitMqTest;
-import com.fun.business.sharon.biz.mq.model.MqMessage;
-import com.fun.business.sharon.common.GlobalResult;
-import com.rabbitmq.client.Channel;
-import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
 
 /**
  * <p>
@@ -38,37 +23,37 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @ApiOperation("测试mq")
-    @GetMapping("/testMq")
-    public GlobalResult<?> testMq(){
-
-        RabbitMqTest rabbitMqTest = new RabbitMqTest();
-        rabbitMqTest.testSend();
-
-//        User user = new User();
-//        userService.save(user);
-        return GlobalResult.newSuccess();
-    }
-
-    @RabbitListener(queues = {Queues.TEST_QUEUE})
-    public void recieveOther(Message message, Channel channel) {
-        try {
-            MqMessage mqMessage = JSON.parseObject(message.getBody(), MqMessage.class);
-            HashMap<String, Object> hashMap = JSON.parseObject(mqMessage.getBody(), HashMap.class);
-
-            String userName = hashMap.get("userName").toString();
-            String address = hashMap.get("address").toString();
-            String email = hashMap.get("email").toString();
-            String phone = hashMap.get("phone").toString();
-            Integer age = Integer.valueOf(hashMap.get("age").toString());
-
-            System.out.println("userName : " + userName + ", address: " + address + ", email: " + email + ", phone: " + phone + ", age: " + age);
-
-            // 给 MQ 确认消息消费，删除队列中的消息
-            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-        }
-    }
+//    @ApiOperation("测试mq")
+//    @GetMapping("/testMq")
+//    public GlobalResult<?> testMq(){
+//
+//        RabbitMqTest rabbitMqTest = new RabbitMqTest();
+//        rabbitMqTest.testSend();
+//
+////        User user = new User();
+////        userService.save(user);
+//        return GlobalResult.newSuccess();
+//    }
+//
+//    @RabbitListener(queues = {Queues.TEST_QUEUE})
+//    public void recieveOther(Message message, Channel channel) {
+//        try {
+//            MqMessage mqMessage = JSON.parseObject(message.getBody(), MqMessage.class);
+//            HashMap<String, Object> hashMap = JSON.parseObject(mqMessage.getBody(), HashMap.class);
+//
+//            String userName = hashMap.get("userName").toString();
+//            String address = hashMap.get("address").toString();
+//            String email = hashMap.get("email").toString();
+//            String phone = hashMap.get("phone").toString();
+//            Integer age = Integer.valueOf(hashMap.get("age").toString());
+//
+//            System.out.println("userName : " + userName + ", address: " + address + ", email: " + email + ", phone: " + phone + ", age: " + age);
+//
+//            // 给 MQ 确认消息消费，删除队列中的消息
+//            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+//        } catch (Exception e) {
+//            log.error(e.getMessage(), e);
+//        }
+//    }
 
 }
--- src/main/java/com/fun/business/sharon/biz/mq/RabbitMqSender.java
+++ src/main/java/com/fun/business/sharon/biz/mq/RabbitMqSender.java
@@ -1,206 +1,206 @@
-package com.fun.business.sharon.biz.mq;
-
-import com.fun.business.sharon.biz.mq.model.MqMessage;
-import com.fun.business.sharon.common.OperateException;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.support.CorrelationData;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
-
-/**
- * MQ 发送消息类
- */
-@Component
-public class RabbitMqSender {
-
-    /** 标准模板 */
-    @Resource
-    private RabbitTemplate rabbitTemplate;
-
-    /** 事物模板 */
-    @Resource
-    private RabbitTemplate transactRabbitTemplate;
-
-    /** confirm模板 */
-    @Resource
-    private RabbitTemplate confirmRabbitTemplate;
-
-    /** return模板 */
-    @Resource
-    private RabbitTemplate returnRabbitTemplate;
-
-    /** confirmAndReturn模板 */
-    @Resource
-    private RabbitTemplate confirmAndReturnRabbitTemplate;
-
-    /** confirmAndReturn抛异常模板 */
-    @Resource
-    private RabbitTemplate confirmAndReturnThrowExceptionRabbitTemplate;
-
-    /**
-     * 功能描述: 发送消息
-     * @param exchange	交换机
-	 * @param routingKey	队列名
-	 * @param message	消息
-     * @Author: Frank
-     * @Date: 2019/6/13
-     * @Version: 0.0.1
-     */
-    public void send(String exchange, String routingKey, Object message) {
-        judgeException(exchange, routingKey);
-        // 发送消息
-        rabbitTemplate.convertAndSend(exchange, routingKey, message);
-    }
-
-    /**
-     * 功能描述: 发送消息
-     *
-     * @param exchange  交换机名称
-     * @param routingKey  队列名
-     * @param mqMessage 队列消息
-     * @Author: Lenly
-     * @Date: 2018/12/31
-     * @Version: 0.0.1
-     **/
-    public void send(String exchange, String routingKey, MqMessage mqMessage) {
-        judgeException(exchange, routingKey);
-        // 发送消息
-        rabbitTemplate.convertAndSend(exchange, routingKey, mqMessage);
-    }
-
-    public void send(String exchange, String routingKey, MqMessage mqMessage, long expiration) {
-        judgeException(exchange, routingKey);
-        // 发送消息
-        rabbitTemplate.convertAndSend(exchange, routingKey, mqMessage, new ExpirationMessagePostProcessor(expiration));
-    }
-
-    /**
-     * 功能描述: 事物机制发送消息
-     *
-     * @param exchange  交换机名称
-     * @param routingKey  队列名
-     * @param mqMessage 队列消息
-     * @Author: Lenly
-     * @Date: 2018/12/31
-     * @Version: 0.0.1
-     **/
-    public void transactSend(String exchange, String routingKey, MqMessage mqMessage) {
-        judgeException(exchange, routingKey);
-        // 发送消息
-        transactRabbitTemplate.convertAndSend(exchange, routingKey, mqMessage);
-    }
-
-    /**
-     * 功能描述: confirm机制发送消息
-     *
-     * @param exchange  交换机名称
-     * @param routingKey  队列名
-     * @param mqMessage 队列消息
-     * @param messageId 发送消息的id(用于消息发送失败后存在数据库的，所以最好是容易辨认的)
-     * @Author: Lenly
-     * @Date: 2018/12/31
-     * @Version: 0.0.1
-     **/
-    public void confirmSend(String exchange, String routingKey, MqMessage mqMessage, String messageId) {
-        judgeException(exchange, routingKey);
-        /** 组装交换机ID */
-        CorrelationData correlationData = new CorrelationData();
-        correlationData.setId(exchange + ":" + routingKey + ":" + messageId);
-        // 发送消息
-        confirmRabbitTemplate.convertAndSend(exchange, routingKey, mqMessage,correlationData);
-    }
-
-    /**
-     * 功能描述: return机制发送消息
-     *
-     * @param exchange  交换机名称
-     * @param routingKey  队列名
-     * @param mqMessage 队列消息
-     * @Author: Lenly
-     * @Date: 2018/12/31
-     * @Version: 0.0.1
-     **/
-    public void returnSend(String exchange, String routingKey, MqMessage mqMessage) {
-        judgeException(exchange, routingKey);
-        // 发送消息
-        returnRabbitTemplate.convertAndSend(exchange, routingKey, mqMessage);
-    }
-
-    public void returnSend(String exchange, String routingKey, MqMessage mqMessage, long expiration) {
-        judgeException(exchange, routingKey);
-        // 发送消息
-        returnRabbitTemplate.convertAndSend(exchange, routingKey, mqMessage, new ExpirationMessagePostProcessor(expiration));
-    }
-
-    /**
-     * 功能描述:  confirm和return机制发送消息
-     *
-     * @param exchange  交换机名称
-     * @param routingKey  队列名
-     * @param mqMessage 队列消息
-     * @param messageId 发送消息的id(用于消息发送失败后存在数据库的，所以最好是容易辨认的)
-     * @Author: Lenly
-     * @Date: 2018/12/31
-     * @Version: 0.0.1
-     **/
-    public void confirmAndReturnSend(String exchange, String routingKey, MqMessage mqMessage, String messageId) {
-        judgeException(exchange, routingKey);
-        /** 组装交换机ID */
-        CorrelationData correlationData = new CorrelationData();
-        correlationData.setId(exchange + ":" + routingKey + ":" + messageId);
-        // 发送消息
-        confirmAndReturnRabbitTemplate.convertAndSend(exchange, routingKey, mqMessage, correlationData);
-    }
-
-    public void confirmAndReturnSend(String exchange, String routingKey, MqMessage mqMessage, String messageId, long expiration) {
-        judgeException(exchange, routingKey);
-        /** 组装交换机ID */
-        CorrelationData correlationData = new CorrelationData();
-        correlationData.setId(exchange + ":" + routingKey + ":" + messageId);
-        // 发送消息
-        confirmAndReturnRabbitTemplate.convertAndSend(exchange, routingKey, mqMessage, new ExpirationMessagePostProcessor(expiration), correlationData);
-    }
-
-    /**
-     * 功能描述:  confirm和return机制发送消息
-     *
-     * @param exchange  交换机名称
-     * @param routingKey  队列名
-     * @param mqMessage 队列消息
-     * @param messageId 发送消息的id(用于消息发送失败后存在数据库的，所以最好是容易辨认的)
-     * @Author: Mike
-     * @Date: 2019/1/11
-     * @Version: 0.0.1
-     **/
-    public void confirmAndReturnThrowExceptionSend(String exchange, String routingKey, MqMessage mqMessage, String messageId) {
-        judgeException(exchange, routingKey);
-        //RabbitTemplate confirmAndReturnRabbitTemplate = SpringUtils.getBean("confirmAndReturnRabbitTemplate",RabbitTemplate.class);
-
-        /** 组装交换机ID */
-        CorrelationData correlationData = new CorrelationData();
-        correlationData.setId(exchange + ":" + routingKey + ":" + messageId);
-        // 发送消息
-        confirmAndReturnThrowExceptionRabbitTemplate.convertAndSend(exchange, routingKey, mqMessage, correlationData);
-    }
-
-    public void confirmAndReturnThrowExceptionSend(String exchange, String routingKey, MqMessage mqMessage, String messageId, long expiration) {
-        judgeException(exchange, routingKey);
-        //RabbitTemplate confirmAndReturnRabbitTemplate = SpringUtils.getBean("confirmAndReturnRabbitTemplate",RabbitTemplate.class);
-
-        /** 组装交换机ID */
-        CorrelationData correlationData = new CorrelationData();
-        correlationData.setId(exchange + ":" + routingKey + ":" + messageId);
-        // 发送消息
-        confirmAndReturnThrowExceptionRabbitTemplate.convertAndSend(exchange, routingKey, mqMessage, new ExpirationMessagePostProcessor(expiration), correlationData);
-    }
-
-    private void judgeException(String exchange, String routingKey){
-        if(null == exchange || "".equals(exchange)){
-            throw new OperateException("交换机不能为空");
-        }
-        if(null == routingKey || "".equals(routingKey)){
-            throw new OperateException("routingKey不能为空");
-        }
-    }
-}
+//package com.fun.business.sharon.biz.mq;
+//
+//import com.fun.business.sharon.biz.mq.model.MqMessage;
+//import com.fun.business.sharon.common.OperateException;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.amqp.rabbit.support.CorrelationData;
+//import org.springframework.stereotype.Component;
+//
+//import javax.annotation.Resource;
+//
+///**
+// * MQ 发送消息类
+// */
+//@Component
+//public class RabbitMqSender {
+//
+//    /** 标准模板 */
+//    @Resource
+//    private RabbitTemplate rabbitTemplate;
+//
+//    /** 事物模板 */
+//    @Resource
+//    private RabbitTemplate transactRabbitTemplate;
+//
+//    /** confirm模板 */
+//    @Resource
+//    private RabbitTemplate confirmRabbitTemplate;
+//
+//    /** return模板 */
+//    @Resource
+//    private RabbitTemplate returnRabbitTemplate;
+//
+//    /** confirmAndReturn模板 */
+//    @Resource
+//    private RabbitTemplate confirmAndReturnRabbitTemplate;
+//
+//    /** confirmAndReturn抛异常模板 */
+//    @Resource
+//    private RabbitTemplate confirmAndReturnThrowExceptionRabbitTemplate;
+//
+//    /**
+//     * 功能描述: 发送消息
+//     * @param exchange	交换机
+//	 * @param routingKey	队列名
+//	 * @param message	消息
+//     * @Author: Frank
+//     * @Date: 2019/6/13
+//     * @Version: 0.0.1
+//     */
+//    public void send(String exchange, String routingKey, Object message) {
+//        judgeException(exchange, routingKey);
+//        // 发送消息
+//        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+//    }
+//
+//    /**
+//     * 功能描述: 发送消息
+//     *
+//     * @param exchange  交换机名称
+//     * @param routingKey  队列名
+//     * @param mqMessage 队列消息
+//     * @Author: Lenly
+//     * @Date: 2018/12/31
+//     * @Version: 0.0.1
+//     **/
+//    public void send(String exchange, String routingKey, MqMessage mqMessage) {
+//        judgeException(exchange, routingKey);
+//        // 发送消息
+//        rabbitTemplate.convertAndSend(exchange, routingKey, mqMessage);
+//    }
+//
+//    public void send(String exchange, String routingKey, MqMessage mqMessage, long expiration) {
+//        judgeException(exchange, routingKey);
+//        // 发送消息
+//        rabbitTemplate.convertAndSend(exchange, routingKey, mqMessage, new ExpirationMessagePostProcessor(expiration));
+//    }
+//
+//    /**
+//     * 功能描述: 事物机制发送消息
+//     *
+//     * @param exchange  交换机名称
+//     * @param routingKey  队列名
+//     * @param mqMessage 队列消息
+//     * @Author: Lenly
+//     * @Date: 2018/12/31
+//     * @Version: 0.0.1
+//     **/
+//    public void transactSend(String exchange, String routingKey, MqMessage mqMessage) {
+//        judgeException(exchange, routingKey);
+//        // 发送消息
+//        transactRabbitTemplate.convertAndSend(exchange, routingKey, mqMessage);
+//    }
+//
+//    /**
+//     * 功能描述: confirm机制发送消息
+//     *
+//     * @param exchange  交换机名称
+//     * @param routingKey  队列名
+//     * @param mqMessage 队列消息
+//     * @param messageId 发送消息的id(用于消息发送失败后存在数据库的，所以最好是容易辨认的)
+//     * @Author: Lenly
+//     * @Date: 2018/12/31
+//     * @Version: 0.0.1
+//     **/
+//    public void confirmSend(String exchange, String routingKey, MqMessage mqMessage, String messageId) {
+//        judgeException(exchange, routingKey);
+//        /** 组装交换机ID */
+//        CorrelationData correlationData = new CorrelationData();
+//        correlationData.setId(exchange + ":" + routingKey + ":" + messageId);
+//        // 发送消息
+//        confirmRabbitTemplate.convertAndSend(exchange, routingKey, mqMessage,correlationData);
+//    }
+//
+//    /**
+//     * 功能描述: return机制发送消息
+//     *
+//     * @param exchange  交换机名称
+//     * @param routingKey  队列名
+//     * @param mqMessage 队列消息
+//     * @Author: Lenly
+//     * @Date: 2018/12/31
+//     * @Version: 0.0.1
+//     **/
+//    public void returnSend(String exchange, String routingKey, MqMessage mqMessage) {
+//        judgeException(exchange, routingKey);
+//        // 发送消息
+//        returnRabbitTemplate.convertAndSend(exchange, routingKey, mqMessage);
+//    }
+//
+//    public void returnSend(String exchange, String routingKey, MqMessage mqMessage, long expiration) {
+//        judgeException(exchange, routingKey);
+//        // 发送消息
+//        returnRabbitTemplate.convertAndSend(exchange, routingKey, mqMessage, new ExpirationMessagePostProcessor(expiration));
+//    }
+//
+//    /**
+//     * 功能描述:  confirm和return机制发送消息
+//     *
+//     * @param exchange  交换机名称
+//     * @param routingKey  队列名
+//     * @param mqMessage 队列消息
+//     * @param messageId 发送消息的id(用于消息发送失败后存在数据库的，所以最好是容易辨认的)
+//     * @Author: Lenly
+//     * @Date: 2018/12/31
+//     * @Version: 0.0.1
+//     **/
+//    public void confirmAndReturnSend(String exchange, String routingKey, MqMessage mqMessage, String messageId) {
+//        judgeException(exchange, routingKey);
+//        /** 组装交换机ID */
+//        CorrelationData correlationData = new CorrelationData();
+//        correlationData.setId(exchange + ":" + routingKey + ":" + messageId);
+//        // 发送消息
+//        confirmAndReturnRabbitTemplate.convertAndSend(exchange, routingKey, mqMessage, correlationData);
+//    }
+//
+//    public void confirmAndReturnSend(String exchange, String routingKey, MqMessage mqMessage, String messageId, long expiration) {
+//        judgeException(exchange, routingKey);
+//        /** 组装交换机ID */
+//        CorrelationData correlationData = new CorrelationData();
+//        correlationData.setId(exchange + ":" + routingKey + ":" + messageId);
+//        // 发送消息
+//        confirmAndReturnRabbitTemplate.convertAndSend(exchange, routingKey, mqMessage, new ExpirationMessagePostProcessor(expiration), correlationData);
+//    }
+//
+//    /**
+//     * 功能描述:  confirm和return机制发送消息
+//     *
+//     * @param exchange  交换机名称
+//     * @param routingKey  队列名
+//     * @param mqMessage 队列消息
+//     * @param messageId 发送消息的id(用于消息发送失败后存在数据库的，所以最好是容易辨认的)
+//     * @Author: Mike
+//     * @Date: 2019/1/11
+//     * @Version: 0.0.1
+//     **/
+//    public void confirmAndReturnThrowExceptionSend(String exchange, String routingKey, MqMessage mqMessage, String messageId) {
+//        judgeException(exchange, routingKey);
+//        //RabbitTemplate confirmAndReturnRabbitTemplate = SpringUtils.getBean("confirmAndReturnRabbitTemplate",RabbitTemplate.class);
+//
+//        /** 组装交换机ID */
+//        CorrelationData correlationData = new CorrelationData();
+//        correlationData.setId(exchange + ":" + routingKey + ":" + messageId);
+//        // 发送消息
+//        confirmAndReturnThrowExceptionRabbitTemplate.convertAndSend(exchange, routingKey, mqMessage, correlationData);
+//    }
+//
+//    public void confirmAndReturnThrowExceptionSend(String exchange, String routingKey, MqMessage mqMessage, String messageId, long expiration) {
+//        judgeException(exchange, routingKey);
+//        //RabbitTemplate confirmAndReturnRabbitTemplate = SpringUtils.getBean("confirmAndReturnRabbitTemplate",RabbitTemplate.class);
+//
+//        /** 组装交换机ID */
+//        CorrelationData correlationData = new CorrelationData();
+//        correlationData.setId(exchange + ":" + routingKey + ":" + messageId);
+//        // 发送消息
+//        confirmAndReturnThrowExceptionRabbitTemplate.convertAndSend(exchange, routingKey, mqMessage, new ExpirationMessagePostProcessor(expiration), correlationData);
+//    }
+//
+//    private void judgeException(String exchange, String routingKey){
+//        if(null == exchange || "".equals(exchange)){
+//            throw new OperateException("交换机不能为空");
+//        }
+//        if(null == routingKey || "".equals(routingKey)){
+//            throw new OperateException("routingKey不能为空");
+//        }
+//    }
+//}
